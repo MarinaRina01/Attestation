da7535dd3696c8d276831de01b62cee05a431e4e
Marina <marina.shubochkina@gmail.com>
2023-04-25 10:37:37 +0300

    Save zd3

---
+++ zd3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int numM = SetNumber(" M");
+int numN = SetNumber(" N");
+int acc = Function(numM, numN);
+Console.WriteLine($"Функция Аккермана A({numM},{numN}) равна {acc}");
+
+int SetNumber(string text)
+{
+    Console.Write($"Введите число{text}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int Function(int numM, int numN)
+{
+    if (numM == 0)
+    {
+        return numN + 1;
+    }
+    else if (numM > 0 && numN == 0)
+    {
+        return Function(numM - 1, 1);
+    }
+    else
+    {
+        return (Function(numM - 1, Function(numM, numN - 1)));
+    }
+}
